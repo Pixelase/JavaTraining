f653ddf44f99c07be9aaaf43f1d87a9919e0fb4d
Alexander Babai <sustem34@gmail.com>
2015-10-31 23:38:19 +0300

    JAVA.SE.04: Fix search algorithm for tasks 1, 2
    Using regex (Pattern and Matcher classes) for now to count the number of matches of keywords in the line;
    It is made in order to avoid wrong matching when one keyword includes another, e.g. "throw" and "throws";
    StringUtils from Apache Commons, that I have used before, can't do this;

--- javase04/src/main/java/javase04/t01/lib/core/AbstractKeywordsAnalyzer.java
+++ javase04/src/main/java/javase04/t01/lib/core/AbstractKeywordsAnalyzer.java
@@ -3,8 +3,8 @@ package javase04.t01.lib.core;
 import java.io.IOException;
 import java.util.LinkedHashMap;
 import java.util.Map;
-
-import org.apache.commons.lang3.StringUtils;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javase04.t01.lib.input.Reader;
 import javase04.t01.lib.output.Writer;
@@ -28,14 +28,22 @@ public abstract class AbstractKeywordsAnalyzer implements Analyser {
 
 	public void analyze() throws IOException {
 		Map<String, Integer> result = new LinkedHashMap<>();
+		Pattern pattern;
+		Matcher matcher;
+		int countMatches = 0;
 
 		for (String line : reader.readAll()) {
 
 			for (String keyword : keywordsHolder.getKeywords()) {
+				pattern = Pattern.compile("\\b" + keyword + "\\b");
+				matcher = pattern.matcher(line);
 
-				int countMatches = (result.containsKey(keyword))
-						? result.get(keyword) + StringUtils.countMatches(line, keyword)
-						: StringUtils.countMatches(line, keyword);
+				countMatches = 0;
+				while (matcher.find()) {
+					countMatches++;
+				}
+
+				countMatches = (result.containsKey(keyword)) ? result.get(keyword) + countMatches : countMatches;
 
 				if (countMatches != 0) {
 					result.put(keyword, countMatches);
