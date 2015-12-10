cbd34486f54cd1b13d7b6d9cad7e95209aaffd09
Alexander Babai <sustem34@gmail.com>
2015-12-11 00:01:06 +0300

    Web-project: add spring beans support into wicket

--- Web-project/webapp/src/main/java/com/github/pixelase/webproject/webapp/app/WicketApplication.java
+++ Web-project/webapp/src/main/java/com/github/pixelase/webproject/webapp/app/WicketApplication.java
@@ -2,23 +2,30 @@ package com.github.pixelase.webproject.webapp.app;
 
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
 
 import com.github.pixelase.webproject.webapp.page.HomePage;
 
 /**
- * Application object for your web application.
- * If you want to run this application without deploying, run the Start class.
+ * Application object for your web application. If you want to run this
+ * application without deploying, run the Start class.
  * 
  * @see сom.github.pixelase.webproject.webapp.Start#main(String[])
  */
-public class WicketApplication extends WebApplication
-{
+@Component("MyWebApplication")
+public class WicketApplication extends WebApplication {
+
+	@Autowired
+	private ApplicationContext context;
+
 	/**
 	 * @see org.apache.wicket.Application#getHomePage()
 	 */
 	@Override
-	public Class<? extends WebPage> getHomePage()
-	{
+	public Class<? extends WebPage> getHomePage() {
 		return HomePage.class;
 	}
 
@@ -26,10 +33,9 @@ public class WicketApplication extends WebApplication
 	 * @see org.apache.wicket.Application#init()
 	 */
 	@Override
-	public void init()
-	{
+	public void init() {
 		super.init();
-
+		getComponentInstantiationListeners().add(new SpringComponentInjector(this, context));
 		// add your configuration here
 	}
 }
--- Web-project/webapp/src/main/java/com/github/pixelase/webproject/webapp/page/HomePage.java
+++ Web-project/webapp/src/main/java/com/github/pixelase/webproject/webapp/page/HomePage.java
@@ -1,18 +1,25 @@
 package com.github.pixelase.webproject.webapp.page;
 
-import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+
+import com.github.pixelase.webproject.services.TenantService;
 
 public class HomePage extends WebPage {
 	private static final long serialVersionUID = 1L;
 
+	@SpringBean 	
+	TenantService service;
+	
 	public HomePage(final PageParameters parameters) {
 		super(parameters);
 
 		add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
 
 		// TODO Add your page's components here
+		System.out.println(service.findAll());
 
     }
 }
