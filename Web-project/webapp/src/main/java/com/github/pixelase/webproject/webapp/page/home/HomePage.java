683f1b896b08367de7fc27865761f4f783ea5623
Alexander Babai <sustem34@gmail.com>
2016-01-11 16:24:59 +0300

    Web-project: decorate home page & fix typography
    * Use to mdl-typography--text-center center typography instead of flex-centered;
    * Prettify home page;

--- Web-project/webapp/src/main/java/com/github/pixelase/webproject/webapp/page/home/HomePage.java
+++ Web-project/webapp/src/main/java/com/github/pixelase/webproject/webapp/page/home/HomePage.java
@@ -1,10 +1,31 @@
 package com.github.pixelase.webproject.webapp.page.home;
 
+import com.github.pixelase.webproject.webapp.app.BasicAuthenticationSession;
 import com.github.pixelase.webproject.webapp.page.base.BasePage;
+import com.github.pixelase.webproject.webapp.page.edit.register.AccountRegisterPage;
+import org.apache.wicket.markup.html.link.Link;
 
 public class HomePage extends BasePage {
 
     public HomePage() {
         super();
+    }
+
+    @Override
+    protected void onInitialize() {
+        super.onInitialize();
+
+        final Link<Void> joinButton = new Link<Void>("join-button") {
+            @Override
+            public void onClick() {
+                setResponsePage(AccountRegisterPage.class);
+            }
+        };
+
+        if (BasicAuthenticationSession.get().isSignedIn()) {
+            joinButton.setVisible(false);
+        }
+
+        add(joinButton);
     }
 }
